e69533a73efa899f6edcdf9be1995198b1ce49f3
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2023-10-25 04:24:41 -0600

    mini update

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,7 +1,107 @@
 import { Link } from "react-router-dom";
-import categories from "../images";
 
+import test from "../Others/base.png";
 import "./Work.css";
+const categories = [
+  {
+    name: "Cartoon Illustrations",
+    folder: "cartoon-ilustrations",
+    images: [
+      {
+        id: "TOKCosplay",
+        name: "Tears of kingdom cosplay",
+        thumbnail: "base.png",
+      },
+      {
+        id: "CharacterStudy1",
+        name: "Character Study 1",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Kallopzi",
+        name: "Kallopzi Cover",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Exploring",
+        name: "Exploring",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Pochita",
+        name: "Pochita!",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Arceus",
+        name: "Legends Arceus Team",
+        thumbnail: "base.png",
+      },
+      {
+        id: "KallopziChar",
+        name: "Kallopzi Characters",
+        thumbnail: "base.png",
+      },
+      {
+        id: "CharacterStudy2",
+        name: "Character Study 2",
+        thumbnail: "base.png",
+      },
+    ],
+  },
+  {
+    name: "Semi-realism",
+    folder: "semi-realism",
+    images: [
+      {
+        id: "Matraka",
+        name: "Matraka",
+        thumbnail: "base.png",
+      },
+      {
+        id: "AsItWas",
+        name: "As It Was",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Queen",
+        name: "Flowers",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Flowers",
+        name: "Queen of Queens",
+        thumbnail: "base.png",
+      },
+    ],
+  },
+  {
+    name: "Videogame Art",
+    folder: "videogame-art",
+    images: [
+      {
+        id: "KALogo",
+        name: "K.A Logo",
+        thumbnail: "base.png",
+      },
+      {
+        id: "KACover",
+        name: "K.A Cover",
+        thumbnail: "base.png",
+      },
+      {
+        id: "KAIcons",
+        name: "K.A Icons",
+        thumbnail: "base.png",
+      },
+      {
+        id: "Portfolio",
+        name: "Portfolio Banner",
+        thumbnail: "base.png",
+      },
+    ],
+  },
+];
 
 const Work = () => (
   <>
@@ -15,7 +115,7 @@ const Work = () => (
               className="image"
               to={`/works/${category.folder}/${image.id}/`}
               style={{
-                backgroundImage: `url(/images/${category.folder}/${image.id}/${image.thumbnail})`,
+                backgroundImage: `url(${test})`,
                 ...(image.backgroundPosition && {
                   backgroundPosition: image.backgroundPosition,
                 }),
